3e109ac66137cdebb52dc3a03d9953def0caf8f5
Annyce Davis <annyce.davis@gmail.com>
2017-12-13 18:46:58 -0500

    [+] 01_04 - Nullability Start

---
+++ app/src/test/java/com/example/solarsystem/Nullability.kt
@@ -0,0 +1,37 @@
+package com.example.solarsystem
+
+import org.junit.Test
+
+class NullabilityTest {
+
+    @Test
+    fun canNotBeNull() {
+
+    }
+
+    @Test
+    fun canBeNull() {
+
+    }
+
+    @Test
+    fun ifNotNullCheck() {
+
+    }
+
+    @Test
+    fun letCheck() {
+
+    }
+
+    @Test
+    fun elvisCheck() {
+
+    }
+
+    @Test
+    fun notNullAssertion() {
+
+    }
+
+}
