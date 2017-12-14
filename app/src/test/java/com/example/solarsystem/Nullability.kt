6de1aee5874992942abc3f9db339d91317f4a3e4
Annyce Davis <annyce.davis@gmail.com>
2017-12-13 20:10:32 -0500

    [+] 01_04 - Nullability Complete

--- app/src/test/java/com/example/solarsystem/Nullability.kt
+++ app/src/test/java/com/example/solarsystem/Nullability.kt
@@ -2,36 +2,58 @@ package com.example.solarsystem
 
 import org.junit.Test
 
+fun doubleString(str: String): String {
+    return str.repeat(2)
+}
+
 class NullabilityTest {
 
     @Test
     fun canNotBeNull() {
+        var flavor: String = "mint"
+//        flavor = null
 
+        println(doubleString(flavor))
     }
 
     @Test
     fun canBeNull() {
+        var flavor: String? = "mint"
+        flavor = null
 
+//        println(doubleString(flavor))
     }
 
     @Test
     fun ifNotNullCheck() {
+        var flavor: String? = "mint"
 
+        if (flavor != null) {
+            println(doubleString(flavor))
+        }
     }
 
     @Test
     fun letCheck() {
+        var flavor: String? = null
 
+        flavor?.let {
+            println(doubleString(flavor))
+        }
     }
 
     @Test
     fun elvisCheck() {
+        var flavor: String? = null
 
+        println(doubleString(flavor ?: "something else"))
     }
 
     @Test
     fun notNullAssertion() {
+        var flavor: String? = null
 
+        println(doubleString(flavor!!))
     }
 
 }
