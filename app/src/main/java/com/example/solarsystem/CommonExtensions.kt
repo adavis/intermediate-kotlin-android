2aa39f0e7c714e8fa24c195f6125113310c9f8c5
Annyce Davis <annyce.davis@gmail.com>
2017-12-14 20:42:50 -0500

    [*] Cleaned up CommonExtensions

--- app/src/main/java/com/example/solarsystem/CommonExtensions.kt
+++ app/src/main/java/com/example/solarsystem/CommonExtensions.kt
@@ -5,16 +5,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.view.View
 
-/**
- * @author Annyce Davis
- */
 fun View.showSnackbar(message: String) {
     Snackbar.make(this, message, Snackbar.LENGTH_LONG)
             .setAction("Action", null).show()
-}
-
-inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
-    beginTransaction().apply{
-        action()
-    }.commit()
 }
