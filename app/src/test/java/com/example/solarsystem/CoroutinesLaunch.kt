c607238af14380f51222ea9b6ec193184c74ee17
Annyce Davis <annyce.davis@gmail.com>
2017-12-20 14:30:43 -0500

    [+] 04_03 - Anko Coroutines Launch Advanced Complete

--- app/src/test/java/com/example/solarsystem/CoroutinesLaunch.kt
+++ app/src/test/java/com/example/solarsystem/CoroutinesLaunch.kt
@@ -1,6 +1,7 @@
 package com.example.solarsystem
 
 import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.CoroutineStart
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import org.junit.Test
@@ -35,12 +36,37 @@ class CoroutinesLaunchTest {
 
     @Test
     fun startWithLaunchMultiple() {
+        println("Starting...")
 
+        launch(CommonPool) {
+            delay(1_000L)
+            println("Inside 1...")
+        }
+
+        launch(CommonPool) {
+            delay(1_000L)
+            println("Inside 2...")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
     @Test
     fun startWithLaunchLazy() {
+        println("Starting...")
 
+        val job = launch(CommonPool, CoroutineStart.LAZY) {
+            delay(1_000L)
+            println("Inside...")
+        }
+
+        job.start()
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
 }
