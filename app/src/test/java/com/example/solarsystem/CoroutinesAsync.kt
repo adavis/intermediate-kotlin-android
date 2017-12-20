aeae8d0a589a6dd14c2bbf4d334b50cda608b467
Annyce Davis <annyce.davis@gmail.com>
2017-12-20 14:36:30 -0500

    [+] 04_04 - Anko Coroutines Async Start

---
+++ app/src/test/java/com/example/solarsystem/CoroutinesAsync.kt
@@ -0,0 +1,22 @@
+package com.example.solarsystem
+
+import org.junit.Test
+
+class CoroutinesAsyncTest {
+
+    @Test
+    fun usingAsync() {
+
+    }
+
+    @Test
+    fun usingAsyncWithDeferred() {
+
+    }
+
+    @Test
+    fun usingAsyncTwice() {
+
+    }
+
+}
