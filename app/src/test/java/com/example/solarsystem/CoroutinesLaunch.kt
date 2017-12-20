9c87aec1abf261136f9d471cb45eefbff6fff449
Annyce Davis <annyce.davis@gmail.com>
2017-12-20 08:42:58 -0500

    [+] 04_02 - Anko Coroutines Launch Start

---
+++ app/src/test/java/com/example/solarsystem/CoroutinesLaunch.kt
@@ -0,0 +1,27 @@
+package com.example.solarsystem
+
+import org.junit.Test
+
+class CoroutinesLaunchTest {
+
+    @Test
+    fun startWithLaunchNoSleep() {
+
+    }
+
+    @Test
+    fun startWithLaunchSleep() {
+
+    }
+
+    @Test
+    fun startWithLaunchMultiple() {
+
+    }
+
+    @Test
+    fun startWithLaunchLazy() {
+
+    }
+
+}
