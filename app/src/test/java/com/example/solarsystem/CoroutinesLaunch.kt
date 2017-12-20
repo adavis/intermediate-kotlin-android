7760b913b1863e4e257fed59b6eec0fb6962d150
Annyce Davis <annyce.davis@gmail.com>
2017-12-20 10:42:39 -0500

    [+] 04_02 - Anko Coroutines Launch Complete

--- app/src/test/java/com/example/solarsystem/CoroutinesLaunch.kt
+++ app/src/test/java/com/example/solarsystem/CoroutinesLaunch.kt
@@ -1,17 +1,36 @@
 package com.example.solarsystem
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import org.junit.Test
 
 class CoroutinesLaunchTest {
 
     @Test
     fun startWithLaunchNoSleep() {
+        println("Starting...")
 
+        launch(CommonPool) {
+            delay(1_000L)
+            println("Inside...")
+        }
+
+        println("Stopping...")
     }
 
     @Test
     fun startWithLaunchSleep() {
+        println("Starting...")
 
+        launch(CommonPool) {
+            delay(1_000L)
+            println("Inside...")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
     @Test
