dead52f814ae1907f6af23660f9b0129c423b33f
Annyce Davis <annyce.davis@gmail.com>
2017-12-20 15:03:41 -0500

    [+] 04_04 - Anko Coroutines Async Complete

--- app/src/test/java/com/example/solarsystem/CoroutinesAsync.kt
+++ app/src/test/java/com/example/solarsystem/CoroutinesAsync.kt
@@ -1,22 +1,66 @@
 package com.example.solarsystem
 
+import kotlinx.coroutines.experimental.*
 import org.junit.Test
 
 class CoroutinesAsyncTest {
 
     @Test
     fun usingAsync() {
+        println("Starting...")
 
+        async(CommonPool) {
+            delay(1_000L)
+            println("Inside...")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
     @Test
     fun usingAsyncWithDeferred() {
+        println("Starting...")
 
+        launch(CommonPool) {
+            val result: Deferred<Int> = async(CommonPool) {
+                delay(1_000L)
+                println("Inside...")
+                42
+            }
+
+            println("the result: ${result.await()}")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
     @Test
     fun usingAsyncTwice() {
+        println("Starting...")
 
+        launch(CommonPool) {
+            val result: Deferred<Int> = async(CommonPool) {
+                delay(1_000L)
+                println("Inside first...")
+                42
+            }
+
+            val result2: Deferred<Int> = async(CommonPool) {
+                delay(1_000L)
+                println("Inside second...")
+                8
+            }
+
+            println("the sum: ${result.await() + result2.await()}")
+        }
+
+        Thread.sleep(1_500L)
+
+        println("Stopping...")
     }
 
 }
