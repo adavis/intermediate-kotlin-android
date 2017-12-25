105d656b7740a5333e8d2d365b6d0de40ea7fad3
Annyce Davis <annyce.davis@gmail.com>
2017-12-25 11:28:16 -0500

    [+] 05_01 - Nullability and Collections Start

---
+++ app/src/test/java/com/example/solarsystem/NullabilityAndCollections.kt
@@ -0,0 +1,27 @@
+package com.example.solarsystem
+
+import org.junit.Test
+
+class NullabilityAndCollectionsTest {
+
+    @Test
+    fun listCannotBeNull() {
+
+    }
+
+    @Test
+    fun listCanBeNull() {
+
+    }
+
+    @Test
+    fun valuesCannotBeNull() {
+
+    }
+
+    @Test
+    fun valuesCanBeNull() {
+
+    }
+
+}
