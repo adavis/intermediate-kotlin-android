62368557d1ec2b14d09f95b6270eb4c16abe184e
Annyce Davis <annyce.davis@gmail.com>
2017-12-26 12:34:20 -0500

    [+] 05_01 - Nullability and Collections Complete

--- app/src/test/java/com/example/solarsystem/NullabilityAndCollections.kt
+++ app/src/test/java/com/example/solarsystem/NullabilityAndCollections.kt
@@ -6,22 +6,32 @@ class NullabilityAndCollectionsTest {
 
     @Test
     fun listCannotBeNull() {
-
+//        var flavors: List<String> = listOf("vanilla", "strawberry")
+//        flavors = null
+//
+//        flavors.forEach { println(it) }
     }
 
     @Test
     fun listCanBeNull() {
+        var flavors: List<String>? = listOf("vanilla", "strawberry")
+        flavors = null
 
+        flavors?.forEach { println(it) }
     }
 
     @Test
     fun valuesCannotBeNull() {
-
+//        var flavors: List<String>? = listOf("vanilla", "strawberry", null)
+//
+//        flavors?.forEach { println(it) }
     }
 
     @Test
     fun valuesCanBeNull() {
+        var flavors: List<String?>? = listOf("vanilla", "strawberry", null)
 
+        flavors?.forEach { println(it) }
     }
 
 }
