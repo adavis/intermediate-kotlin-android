09c84022d5e03d8d38d97d3095a0d43b5ba483fd
Annyce Davis <annyce.davis@gmail.com>
2017-12-28 11:27:50 -0500

    [+] 05_06 - Collections: Aggregate Start

---
+++ app/src/test/java/com/example/solarsystem/AggregateOperations.kt
@@ -0,0 +1,30 @@
+package com.example.solarsystem
+
+import com.example.solarsystem.dummy.PlanetsDataProvider
+import org.junit.Test
+
+class AggregateOperationsTest {
+
+    private val planets = PlanetsDataProvider.ITEMS
+
+    @Test
+    fun fewestMoons() {
+
+    }
+
+    @Test
+    fun planetWithFewestMoons() {
+
+    }
+
+    @Test
+    fun planetWithMostMoons() {
+
+    }
+
+    @Test
+    fun totalMoons() {
+
+    }
+
+}
